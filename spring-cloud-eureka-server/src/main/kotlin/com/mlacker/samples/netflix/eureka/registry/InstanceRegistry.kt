d7a098387881bd85f855cff873be20e5f02c71e9
mlacker <mlacker@163.com>
2021-12-09 15:29:13 +0800

    feat: InstanceRegistry and PeerAwareInstanceRegistry

---
+++ spring-cloud-eureka-server/src/main/kotlin/com/mlacker/samples/netflix/eureka/registry/InstanceRegistry.kt
@@ -0,0 +1,48 @@
+package com.mlacker.samples.netflix.eureka.registry
+
+import com.mlacker.samples.netflix.appinfo.ApplicationInfoManager
+import com.mlacker.samples.netflix.appinfo.InstanceInfo
+import com.mlacker.samples.netflix.appinfo.InstanceStatus
+import com.mlacker.samples.netflix.discovery.shared.Application
+import com.mlacker.samples.netflix.discovery.shared.LookupService
+import com.mlacker.samples.netflix.eureka.lease.LeaseManager
+import com.netflix.eureka.registry.ResponseCache
+
+interface InstanceRegistry : LeaseManager<InstanceInfo>, LookupService {
+
+    fun openForTraffic(applicationInfoManager: ApplicationInfoManager, count: Int)
+
+    fun shutdown()
+
+    fun storeOverriddenStatusIfRequired(appName: String, id: String, overriddenStatus: InstanceStatus)
+
+    fun statusUpdate(appName: String, id: String, newStatus: InstanceStatus, lastDirtyTimestamp: String, isReplication: Boolean): Boolean
+
+    fun deleteStatusOverride(appName: String, id: String, newStatus: InstanceStatus, lastDirtyTimestamp: String, isReplication: Boolean): Boolean
+
+    fun overriddenInstanceStatusesSnapshot(): Map<String, InstanceStatus>
+
+    fun getStoredApplications(): List<Application>
+
+    fun getInstanceByAppAndId(appName: String, id: String): InstanceInfo
+
+    fun clearRegistry()
+
+    fun initializedResponseCache()
+
+    fun getResponseCache(): ResponseCache
+
+    fun getNumOfRenewsInLastMin(): Long
+
+    fun getNumOfRenewsPerMinThreshold(): Int
+
+    fun isBelowRenewThreshold()
+
+    fun getLastNRegisteredInstances(): List<Pair<Long, String>>
+
+    fun getLastNCanceledInstances(): List<Pair<Long, String>>
+
+    fun isLeaseExpirationEnabled(): Boolean
+
+    fun isSelfPreservationModeEnabled(): Boolean
+}
--- spring-cloud-eureka-server/src/main/kotlin/com/mlacker/samples/netflix/eureka/registry/PeerAwareInstanceRegistry.kt
+++ spring-cloud-eureka-server/src/main/kotlin/com/mlacker/samples/netflix/eureka/registry/PeerAwareInstanceRegistry.kt
@@ -1,48 +1,18 @@
 package com.mlacker.samples.netflix.eureka.registry
 
-import com.mlacker.samples.netflix.appinfo.ApplicationInfoManager
 import com.mlacker.samples.netflix.appinfo.InstanceInfo
-import com.mlacker.samples.netflix.appinfo.InstanceStatus
-import com.mlacker.samples.netflix.discovery.shared.Application
-import com.mlacker.samples.netflix.discovery.shared.LookupService
-import com.mlacker.samples.netflix.eureka.lease.LeaseManager
-import com.netflix.eureka.registry.ResponseCache
+import com.netflix.eureka.cluster.PeerEurekaNodes
+import com.netflix.eureka.resources.ASGResource
 
-interface PeerAwareInstanceRegistry : LeaseManager<InstanceInfo>, LookupService {
+interface PeerAwareInstanceRegistry : InstanceRegistry {
 
-    fun openForTraffic(applicationInfoManager: ApplicationInfoManager, count: Int)
+    fun init(peerEurekaNodes: PeerEurekaNodes)
 
-    fun shutdown()
+    fun syncUp(): Int
 
-    fun storeOverriddenStatusIfRequired(appName: String, id: String, overriddenStatus: InstanceStatus)
+    fun shouldAllowAccess(): Boolean
 
-    fun statusUpdate(appName: String, id: String, newStatus: InstanceStatus, lastDirtyTimestamp: String, isReplication: Boolean): Boolean
+    fun register(info: InstanceInfo, isReplication: Boolean)
 
-    fun deleteStatusOverride(appName: String, id: String, newStatus: InstanceStatus, lastDirtyTimestamp: String, isReplication: Boolean): Boolean
-
-    fun overriddenInstanceStatusesSnapshot(): Map<String, InstanceStatus>
-
-    fun getStoredApplications(): List<Application>
-
-    fun getInstanceByAppAndId(appName: String, id: String): InstanceInfo
-
-    fun clearRegistry()
-
-    fun initializedResponseCache()
-
-    fun getResponseCache(): ResponseCache
-
-    fun getNumOfRenewsInLastMin(): Long
-
-    fun getNumOfRenewsPerMinThreshold(): Int
-
-    fun isBelowRenewThreshold()
-
-    fun getLastNRegisteredInstances(): List<Pair<Long, String>>
-
-    fun getLastNCanceledInstances(): List<Pair<Long, String>>
-
-    fun isLeaseExpirationEnabled(): Boolean
-
-    fun isSelfPreservationModeEnabled(): Boolean
+    fun statusUpdate(asgName: String, newStatus: ASGResource.ASGStatus, isReplication: Boolean)
 }
