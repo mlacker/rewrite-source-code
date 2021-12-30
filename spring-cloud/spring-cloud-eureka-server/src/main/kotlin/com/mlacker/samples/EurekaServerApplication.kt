52323f1ebc2352f5ed8aa6642876ff1881ee647b
mlacker <mlacker@163.com>
2021-12-30 15:21:36 +0800

    refactor: spring-cloud-eureka

--- spring-cloud/spring-cloud-eureka-server/src/main/kotlin/com/mlacker/samples/EurekaServerApplication.kt
+++ spring-cloud/spring-cloud-eureka-server/src/main/kotlin/com/mlacker/samples/EurekaServerApplication.kt
@@ -2,11 +2,7 @@ package com.mlacker.samples
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
-import org.springframework.cloud.client.SpringCloudApplication
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
 
 @SpringBootApplication
 @EnableEurekaServer
